ec04810597c38d19da09835d8f441d0a714973f5
AlexNog <ruben_nogueiragarcia@hotmail.com>
2025-06-05 16:44:21 -0600

    New env variable configured

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os 
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+from pydantic import ConfigDict
+
 
 #load variables form .env file
 load_dotenv()
@@ -14,6 +16,20 @@ class Settings(BaseSettings):
     APP_ENVIROMENT: str = os.getenv("APP_ENVIROMENT", "development")
     DEBUG: bool = os.getenv("DEBUG","True").lower() == "true"
 
+    # DB configuration
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://usuario:contraseña@localhost:5432/app_db")
+    
+    # Security configuration
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "clave_secreta_predeterminada")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+    REFRESH_TOKEN_EXPIRE_HOURS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_HOURS", "24"))
+    
+    model_config = ConfigDict(
+        env_file=".env",
+        case_sensitive=True,
+        extra="ignore"  # Ignora variables no definidas en la clase
+    )
 
 
 
