d81875a57d1f7c283d01f4abfe577124586e9b93
AlexNog15 <ruben_nogueiragarcia@hotmail.com>
2025-07-11 16:20:49 -0600

    added new endpoints: 'validate', 'refresh', 'logout', 'public-key'

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.schemas.user import UserCreate
 from app.services.user import create_user
-from app.schemas.auth import Login, Token
-from app.services.auth import generate_user_token, generate_user_login
+from app.schemas.auth import Login, Token, ValidateData, TokenRequest, RefreshTokenRequest
+from app.services.auth import generate_user_token, generate_user_login, get_user_token, renovate_access_token, get_current_active_user, user_logout, get_public_key
 
 router = APIRouter(
     prefix="/api/auth",
@@ -50,3 +50,53 @@ async def get_token(
     
     access_token , refresh_token = generate_user_token(db, form_data)
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+@router.post("/validate", response_model=ValidateData, status_code=status.HTTP_200_OK)
+async def validate_token(
+    token: TokenRequest,
+    db: Session = Depends(get_db)
+):
+    """
+    recieves and validates user
+    """
+    user = get_user_token(db, token)
+
+    return {"user": user}
+
+@router.post("/refresh", response_model=Token, status_code=status.HTTP_200_OK)
+async def get_access_token(
+    token: RefreshTokenRequest,
+    db: Session = Depends(get_db)
+):
+    """
+    renovate access token
+    """
+    access_token , refresh_token = renovate_access_token(db, token)
+
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+@router.post("/logout", status_code=status.HTTP_200_OK)
+async def logout_user(
+    current_user = Depends(get_current_active_user),
+    db: Session = Depends(get_db)
+):
+    """
+    logout the user and invalidate all refreshtoken associate to the user
+    """
+    user_logout(db, current_user)
+
+    return {"message": "Logout successful"}
+
+@router.get("/public-key", status_code=status.HTTP_200_OK)
+async def public_key():
+    pblc_key = get_public_key()
+
+    if not pblc_key:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="No se encontro la llave pública",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    
+    
+    return {"public_key": pblc_key.replace("\n", "")}
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,14 +1,19 @@
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from typing import Optional
 from passlib.context import CryptContext
-from fastapi import HTTPException,status
+from fastapi import HTTPException,status, Depends
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 
+from app.database import get_db
 from app.models.user import User
 from app.config import settings
 from app.models.refresh_token import RefreshToken
+from app.schemas.auth import TokenRequest, RefreshTokenRequest
+
+#OAuth2 configuration
+oauth2_scheme  = OAuth2PasswordBearer(tokenUrl = "api/auth/token")
 
 #configuring bcrypt for password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -38,6 +43,17 @@ def get_private_key():
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Error reading private key"
         )
+
+def get_public_key():
+    """Read the public key from file"""
+    try:
+        with open(settings.RSA_PUBLIC_KEY_PATH, 'r') as f:
+            return f.read()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error reading public key"
+        )    
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     """Create a JWT token with the provided data"""
@@ -140,3 +156,143 @@ def generate_user_token(db: Session, form_data: OAuth2PasswordRequestForm) -> tu
     )
 
     return access_token, refresh_token
+
+def get_user_token(db: Session, token: TokenRequest) -> User:
+    try:
+        acces_token = token.access_token
+        private_key = get_private_key()
+        decoded_jwt = jwt.decode(acces_token, private_key, algorithms=settings.ALGORITHM)
+        sub = decoded_jwt.get("sub")
+
+        user= db.query(User).filter(User.username == sub).first()
+
+        if not user or not user.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="El token no es válido",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        
+        return user
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="El token no es válido",
+                headers={"WWW-Authenticate": "Bearer"},
+        )
+
+def renovate_access_token(db:Session , token: RefreshTokenRequest) -> tuple[str, str]:
+
+    refresh_token_request = token.refresh_token
+
+    db_refresh_token = db.query(RefreshToken).filter(
+        RefreshToken.token == refresh_token_request,
+        RefreshToken.is_active == True                                           
+        ).first()
+
+    if not db_refresh_token:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="El  refresh token no es valido o no esta activo",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    private_key = get_private_key()
+    decoded_jwt = jwt.decode(refresh_token_request, private_key, algorithms=settings.ALGORITHM)
+    sub = decoded_jwt.get("sub")
+
+    user= db.query(User).filter(User.username == sub).first()
+
+    if not user or not user.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="El token no es válido",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    refresh_token_expires = timedelta(hours=settings.REFRESH_TOKEN_EXPIRE_HOURS)
+
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires
+    )
+    refresh_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=refresh_token_expires
+    )
+    
+    db_refresh_token.is_active = False
+    
+
+    db_token = RefreshToken(
+        user_id=user.id,
+        token=refresh_token,
+        expires_at=datetime.now(timezone.utc) + refresh_token_expires
+    )
+
+    db.add(db_token)
+    db.commit()
+    db.refresh(db_token)
+
+    return access_token, refresh_token
+
+def get_current_user(db:Session, token: str) -> User:
+    
+    try:
+        private_key = get_private_key()
+        decoded_jwt = jwt.decode(token, private_key, algorithms=settings.ALGORITHM)
+        username = decoded_jwt.get("sub")
+
+        if not username:
+             raise HTTPException(
+                  status_code=status.HTTP_401_UNAUTHORIZED,
+                  detail="token invalido"
+             )
+
+    except JWTError:
+         raise HTTPException(
+              status_code=status.HTTP_401_UNAUTHORIZED,
+              detail="token invalido"
+         ) 
+    
+    user= db.query(User).filter(User.username == username).first()
+    
+    return user
+
+def get_current_active_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+    ) -> User:
+
+    current_user = get_current_user(db,token)
+
+    if not current_user or not current_user.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="usuario inactivo",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    return current_user
+    
+def user_logout(db: Session, current_user: User):
+
+    refresh_token = db.query(RefreshToken).filter(
+        RefreshToken.user_id == current_user.id,
+        RefreshToken.is_active == True 
+        ).all()
+    
+    if not refresh_token:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="token invalido o no existe",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    
+    for token in refresh_token:
+        token.is_active = False
+        token.revoked_at = datetime.now(timezone.utc)
+
+    db.commit()
+    return
